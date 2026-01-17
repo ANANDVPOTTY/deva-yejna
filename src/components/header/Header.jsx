63012ad47577288866fb96e35add47ca437ffdc6
Anand V Potty <anandvpotty@gmail.com>
2026-01-17 18:56:27 +0530

    Initial Setup & Header Component.

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import MenuIcon from "@mui/icons-material/Menu";
+import {
+  StyledAppBar,
+  StyledToolbar,
+  LogoContainer,
+  LogoImage,
+  LogoText,
+  NavContainer,
+  NavItem,
+  HamburgerButton,
+} from "./Header.styles";
+import RespoNavBar from "./RespoNavBar";
+import logo from "../../assets/images/logo.png";
+
+const NAV_ITEMS = [
+  { label: "About", href: "/about" },
+  { label: "Gallery", href: "/gallery" },
+  { label: "Contact Us", href: "/contact" },
+];
+
+const Header = () => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const handleDrawerOpen = () => setDrawerOpen(true);
+  const handleDrawerClose = () => setDrawerOpen(false);
+
+  return (
+    <StyledAppBar>
+      <StyledToolbar>
+        <LogoContainer component={Link} to="/">
+          <LogoImage src={logo} alt="Deva Yejna Logo" />
+          <LogoText>Deva Yejna</LogoText>
+        </LogoContainer>
+
+        <NavContainer component="nav">
+          {NAV_ITEMS.map((item) => (
+            <NavItem key={item.label} href={item.href} disableRipple>
+              {item.label}
+            </NavItem>
+          ))}
+        </NavContainer>
+
+        <HamburgerButton onClick={handleDrawerOpen} aria-label="open menu">
+          <MenuIcon />
+        </HamburgerButton>
+
+        <RespoNavBar open={drawerOpen} onClose={handleDrawerClose} />
+      </StyledToolbar>
+    </StyledAppBar>
+  );
+};
+
+export default Header;
---
+++ src/components/header/RespoNavBar.jsx
@@ -0,0 +1,60 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { Drawer, List, ListItem, ListItemText, IconButton } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import {
+  DrawerContent,
+  DrawerHeader,
+  DrawerLogoContainer,
+  DrawerLogoImage,
+  DrawerLogoText,
+  DrawerNavItem,
+} from "./Header.styles";
+import logo from "../../assets/images/logo.png";
+
+const NAV_ITEMS = [
+  { label: "About", href: "/about" },
+  { label: "Gallery", href: "/gallery" },
+  { label: "Contact Us", href: "/contact" },
+];
+
+const RespoNavBar = ({ open, onClose }) => {
+  return (
+    <Drawer anchor="right" open={open} onClose={onClose}>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerLogoContainer>
+            <DrawerLogoImage src={logo} alt="Deva Yejna Logo" />
+            <DrawerLogoText>Deva Yejna</DrawerLogoText>
+          </DrawerLogoContainer>
+          <IconButton onClick={onClose} sx={{ color: "var(--color-charcoal)" }}>
+            <CloseIcon />
+          </IconButton>
+        </DrawerHeader>
+
+        <List>
+          {NAV_ITEMS.map((item) => (
+            <ListItem key={item.label} disablePadding>
+              <DrawerNavItem component={Link} to={item.href} onClick={onClose}>
+                <ListItemText
+                  primary={item.label}
+                  primaryTypographyProps={{
+                    fontFamily: "var(--font-primary)",
+                    fontWeight: 500,
+                  }}
+                />
+              </DrawerNavItem>
+            </ListItem>
+          ))}
+        </List>
+      </DrawerContent>
+    </Drawer>
+  );
+};
+
+RespoNavBar.propTypes = {
+  open: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
+
+export default RespoNavBar;
